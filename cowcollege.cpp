7ff53b3a487a103764b251a1c734b428b70788e4
Aldric Benalan <69468612+AstroPixel24@users.noreply.github.com>
2023-11-17 17:32:04 -0500

    Create cowcollege.cpp

---
+++ cowcollege.cpp
@@ -0,0 +1,29 @@
+// USACO 2022 December Contest Bronze - Problem 1. Cow College
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    long long arr[n];
+    for (int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr+n);
+
+    long long max = 0;
+    int store;
+    
+    for (int i=0; i<=n-1; i++) {
+        long long total = (n-i) * arr[i];
+           
+        if (total > max) {
+            max = total;
+            store = arr[i];
+        }
+    }
+       
+    cout << max << " " << store;
+}
