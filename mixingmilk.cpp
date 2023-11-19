a6ba3e3a0804f8d77984ea77ff0eef2c0f291bd2
Aldric Benalan <69468612+AstroPixel24@users.noreply.github.com>
2023-11-19 01:53:33 -0500

    Update mixingmilk.cpp

--- mixingmilk.cpp
+++ mixingmilk.cpp
@@ -1,44 +1,50 @@
+// USACO 2018 December Contest, Bronze - Problem 1. Mixing Milk
+
 #include <iostream>
 using namespace std;
 
 int main() {
     freopen("mixmilk.in", "r", stdin);
-    freopen("mixmilk.out", "w", stdout);
+	freopen("mixmilk.out", "w", stdout);
 
-    int c1, m1, c2, m2, c3, m3;
-    cin >> c1 >> m1 >> c2 >> m2 >> c3 >> m3;
-
-    for (int i=0; i<33; i++){
-        if (m1 <= c2-m2) {
-            m2 = m2 + m1;
-            m1 = 0;
+    int a, b, c;
+    int c1, c2, c3;
+    cin >> c1 >> a;
+    cin >> c2 >> b;
+    cin >> c3 >> c;
+    
+    for (int i=0; i<33; i++) {
+        if (a+b>c2) {
+            a=a-(c2-b);
+            b=c2;
         } else {
-            m1 = abs(m1-c2+m2); 
-            m2 = c2;
-        }
-         if (m2 <= c3-m3) {
-            m3 = m3 + m2;
-            m2 = 0;
-        } else {
-            m2 = abs(m2-c3+m3);
-            m3 = c3;
+            b=a+b;
+            a=0;
         } 
-        if (m3 <= c1-m1) {
-            m1 = m1 + m3;
-            m3 = 0;
+        
+        if (b+c>c3) {
+            b=b-(c3-c);
+            c=c3;
         } else {
-            m3 = abs(m3-c1+m1);
-            m1 = c1;
+            c=b+c;
+            b=0;
+        }
+        
+        if (c+a>c1) {
+            c=c-(c1-a);
+            a=c1;
+        } else {
+            a=c+a;
+            c=0;
         }
     }
-        if (m1 <= c2-m2) {
-            m2 = m2 + m1;
-            m1 = 0;
-        } else {
-            m1 = abs(m1-c2+m2); 
-            m2 = c2;
-        }
-
-
-    cout << m1 << "\n" << m2 << "\n" << m3;
+    if (a+b>c2) {
+        a=a-(c2-b);
+        b=c2;
+    } else {
+        b=a+b;
+        a=0;
+    }
+    
+    cout << a << endl << b << endl << c;
 }
