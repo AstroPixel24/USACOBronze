5b2f1dc7872ac9e8a05800da4227ddea84392621
Aldric Benalan <69468612+AstroPixel24@users.noreply.github.com>
2023-11-27 22:12:23 -0500

    Cleaner solution

--- wordprocessor.cpp
+++ wordprocessor.cpp
@@ -1,29 +1,31 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
+// USACO 2020 January Contest, Bronze - Problem 1. Word Processor
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-  freopen("word.in", "r", stdin);
-  freopen("word.out", "w", stdout);
-  
-  int N, K;
-  cin >> N >> K;
+    freopen("word.in", "r", stdin);
+    freopen("word.out", "w", stdout);
 
-  int sentence = 0;
-  for (int i = 0; i < N; i++) {
-    string word;
-    cin >> word;
-    sentence = sentence + word.length();
-    if (sentence > K) {
-      cout << "\n" << word;
-      sentence = word.length();
-    } else {
-      if (i != 0) {
-        cout << ' ';
-      }
-      cout << word;
+    int n, k;
+    cin >> n >> k;
+    string arr[n];
+    for (int i=0; i<n; i++) {
+        cin >> arr[i];
     }
-  }
+
+    int remain = k;
+    for (int i=0; i<n; i++) {
+        if (remain >= arr[i].length()) {
+            if (i != 0) {
+                cout << " ";
+            }
+            cout << arr[i];
+            remain = remain - arr[i].length();
+        } else {
+            cout << endl;
+            cout << arr[i];
+            remain = k - arr[i].length();
+        }
+    }
 }
