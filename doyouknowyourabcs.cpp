2e2032c51be3614e6cdcc8d96b2464b46e92414d
Aldric Benalan <69468612+AstroPixel24@users.noreply.github.com>
2023-11-27 22:13:10 -0500

    Better formatted solution

--- doyouknowyourabcs.cpp
+++ doyouknowyourabcs.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-#include <algorithm>
+// USACO 2020 December Contest, Bronze - Problem 1. Do You Know Your ABCs?
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-  int arr[7];
-  for (int i=0; i<7; i++) {
-    cin >> arr[i];
-  }
-  sort(arr,arr+7);
-  cout << arr[0] << " ";
-  cout << arr[1] << " ";
-  cout << arr[6] - arr[0] - arr[1];
-
+    int arr[7];
+    for (int i=0; i<7; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr+7);
+    cout << arr[0] << " "; // min
+    cout << arr[1] << " "; // second smallest
+    cout << arr[6]-arr[1]-arr[0]; // c = (a+b+c)-a-b
 }
